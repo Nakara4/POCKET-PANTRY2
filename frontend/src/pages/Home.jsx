9809f2188df3fa8d2e7ef6a5fedca3100522aa01
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-05-28 16:51:08 +0300

    firebaseAuth done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
+// src/App.jsx
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import React from 'react';
-
+import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
