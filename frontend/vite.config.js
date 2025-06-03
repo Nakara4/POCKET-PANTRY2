1d80b4fba092922142111a5f3fa026f6dc67d72f
Dennis Karanja <ngangapepe8@gmail.com>
2025-06-03 11:14:05 +0300

    updated vite.cofig2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  base: '/POCKET-PANTRY2/', 
+  base: '/POCKET-PANTRY2/',
   plugins: [
-    react(), // 
+    react(),
+    tailwindcss(),
   ],
   build: {
     outDir: 'dist',
