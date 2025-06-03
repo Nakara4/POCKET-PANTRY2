3fc26a64fdd9591b04d3f4206d878c104db26ed7
Aron Onkware <aronondieki045@gmail.com>
2025-06-03 13:41:30 +0300

    Update recipes.py

--- backend/routers/recipes.py
+++ backend/routers/recipes.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,10 +10,12 @@ API_KEY = os.getenv("SPOONACULAR_API_KEY")
 
 @router.get("/")
 def get_recipes(number: int = 10):
+    if not API_KEY:
+        raise HTTPException(status_code=500, detail="Spoonacular API key not found in environment variables.")
     url = "https://api.spoonacular.com/recipes/random"
     params = {
         "apiKey": API_KEY,
-        "number": 40,
+        "number": number,
     }
     try:
         response = requests.get(url, params=params)
