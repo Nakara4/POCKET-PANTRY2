6a36a4350421edbceaad9c9fe30fd3eeb222c073
Dennis Nganga <ngangapepe8@gmail.com>
2025-06-03 09:03:35 +0300

    Added github pages link to main.py (#44)
    * Fix homepage case sensitivity
    
    * Force clean build with updated base path
    
    * solved balck page (#41)
    
    * Fix homepage case sensitivity (#38)
    
    * Fix homepage case sensitivity (#39)
    
    * About (#40)
    
    * Fix homepage case sensitivity
    
    * Force clean build with updated base path
    
    * updated main.yml and home js
    
    * Added github pages link in main.py

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS middleware (allow frontend requests)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change this to your frontend domain in production
+    allow_origins=["https://nakara4.github.io/POCKET-PANTRY2/"],  # Change this to your frontend domain in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
